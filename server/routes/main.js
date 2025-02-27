b06ea264a72e60cbebcecd70d496a1a6fc85cb1e
Nahyaanp <nahyaanp@gmail.com>
2025-02-27 21:35:46 +0000

    Connected it with the DataBase

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 
 //Routes
 router.get('' , (req, res) => {
 
     const locals = {
-        title: "NodeJs Blog",
+        title: "The Heritage Gazette",
         description: "Simple Blog Created with NodeJs, Express & Mongo DB."
     }
     res.render('index', {locals});
 });
+
+
+
+
+
+function insertPostData () {
+    Post.insertMany([
+        {
+            title:"Building a Blog",
+            description:"This is the body text"
+        },
+    ])
+}
+
+insertPostData();
 
 router.get('/about' , (req, res) => {
     res.render('about');
