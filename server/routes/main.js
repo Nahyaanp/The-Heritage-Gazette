271d166529af008ace94c764a7b1d6f5bb37304c
Nahyaanp <nahyaanp@gmail.com>
2025-02-27 18:41:54 +0000

    Created Header and Footer

---
+++ server/routes/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Routes
+router.get('' , (req, res) => {
+
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog Created with NodeJs, Express & Mongo DB."
+    }
+    res.render('index', {locals});
+});
+
+router.get('/about' , (req, res) => {
+    res.render('about');
+});
+
+module.exports = router;
